902e27734e515c8108362507ae2712c2975911f5
Naveen Ramakrishna <naveenrk18@gmail.com>
2022-02-11 14:22:12 +0530

    Problem/inverse a number (#5)
    * 1 file added Inverse a number
    * 1 file modified Updated the logic

--- inverseNumber.js
+++ inverseNumber.js
@@ -1,20 +1,19 @@
 /**
  * Inverse a Number
  */
-const number = 426135;
-function inverseANumber(number) {
-  console.log(number);
-  console.log(Array.from(String(number), Number));
-  const arr = Array.from(String(number), Number);
-  const length = arr.length;
-  let newArr = [];
-  let sum = 0;
-  for (let i = 1; i <= length; i++) {
-    let val = Math.pow(10, arr[length - i]) * i;
-    sum += val;
-    newArr.push(val);
-  }
-
-  console.log(sum);
-}
-inverseANumber(number);
+ const number = 426135;
+ function inverseANumber(number) {
+   console.log(number);
+   console.log(Array.from(String(number), Number));
+   const arr = Array.from(String(number), Number);
+   const length = arr.length;
+   let sum = 0;
+   for (let i = 1; i <= length; i++) {
+     let val = Math.pow(10, arr[length - i]) * i;
+     sum += val / 10;
+   }
+ 
+   return sum;
+ }
+ const value = inverseANumber(number);
+ console.log(`Inverse of ${number} is : ${value}`);
